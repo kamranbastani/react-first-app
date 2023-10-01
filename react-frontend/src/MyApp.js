7803d3f8045b21cf2af539ca81498e290bf20ffb
kamranbastani <kmbastan@calpoly.edu>
2023-09-30 17:06:02 -0700

    through step 8

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -32,14 +32,21 @@ function MyApp() {
         setCharacters(updated);
     }
 
+    function updateList(person) {
+        setCharacters([...characters, person]);
+    }
+
+
     return (
         <div className="container">
             <Table characterData={characters}
                    removeCharacter={removeOneCharacter} />
-            <Form />
+            <Form handleSubmit={updateList} />
         </div>
     )
 }
+
+
 
 
 
